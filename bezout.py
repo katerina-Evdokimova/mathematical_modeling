52707d6d6c5c9d1dc61d7c1bc3c54d5a5663746a
katerina-Evdokimova <eea2002_2107leto@mail.ru>
2022-06-05 00:38:05 +0300

    added comments

--- bezout.py
+++ bezout.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from math import sqrt, nan
 
 import numpy as np
 
@@ -6,14 +6,32 @@ from core import Polynomial
 from trinomial import QuadraticTrinomial
 from scipy.optimize import root
 
+"""
 
+"""
+
+
+# NEED TO FIX
+# A code with a stable error is required
+# ------------------------------------------------------------------------------------------------------------
+# This method allows you to determine whether there is a necessary root in the found list
+# The input receives:
+#               divisors - a list with prime numbers;
+#               polynomial - the original polynomial in which
+#                            it will be necessary to check the numbers from the specified list.
+#                            (the data type is a Polynomial.)
+# The output is: one root of a given polynomial is obtained.
+#         If the root is not found, None is returned.
 def _decision(divisors, polynomial: Polynomial):
-    result = root(polynomial, np.array([0.]))
-    if abs(np.round(result.x, 8)[0]) in divisors:
-        return np.round(result.x, 8)[0]
+    result = np.round(root(polynomial, np.array([0.])).x, 8)[0]
+    if abs(result) in divisors:
+        return result
     return None
 
 
+# The method allows you to find the prime divisors of a number
+# The input is: one integer
+# The output is: a list with prime divisors of the input number.
 def _divisors(n):
     result = set()
     for i in range(1, int(sqrt(n)) + 1):
@@ -23,6 +41,11 @@ def _divisors(n):
     return list(result)
 
 
+# The method allows you to find the roots of a polynomial based on the Buzout method.
+# (The description is given in readme.md )
+# The input is: polynomial the data type is a Polynomial.
+# The output is: a root (a floating-point number);
+#                a polynomial with a reduced degree obtained by dividing by x - root (the data type is a polynomial).
 def root_polynomial(polynomial: Polynomial):
     if sum(polynomial) == 0:
         return 1, polynomial // Polynomial(1, -1)
@@ -37,6 +60,9 @@ def root_polynomial(polynomial: Polynomial):
             return None, None
 
 
+# This method implements the solution of the polynomial using the Buzu method.
+# The input is: polynomial the data type is a Polynomial.
+# The output is: a list of roots
 def bezout(polynomial: Polynomial):
     roots = []
     if len(polynomial) != 4:
@@ -45,7 +71,7 @@ def bezout(polynomial: Polynomial):
         polynomial /= polynomial[4]
     # if polynomial[4] == 0:
     #     return ZeroDivisionError("Can't divide a Polynomial by 0")
-        # print(polynomial)
+    # print(polynomial)
     root, polynomial2 = root_polynomial(polynomial)
     roots.append(root)
     if polynomial2:
@@ -59,4 +85,8 @@ def bezout(polynomial: Polynomial):
     return roots
 
 
+a = Polynomial(1, 3, 7, -21, -26)
+a *= 3
+a /= 3
+print(a // Polynomial(1, -1))
 print(bezout(Polynomial(1, 3, 7, -21, -26)))  # [-1, 2, (-2+3j), (-2-3j)]
